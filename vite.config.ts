43f4e7aa0d4e0d80eb8bca4a3482af88e88f8168
John Denk <johndenk19@gmail.com>
2026-01-12 22:02:23 -0500

    ahh yeah

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8080,
+    host: true // Needed for Docker to expose the port correctly
+  }
   // Usually, no 'base' is needed unless you're serving from a subfolder
 })
